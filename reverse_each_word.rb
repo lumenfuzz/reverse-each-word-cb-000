eab8d0b9b76bdeeddc77df5b8687514180e2a5bf
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-12 15:01:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   new_string = ""
-  array.each do |element|
+  array.collect do |element|
     new_string << element.reverse
     new_string << " "
   end
